ef0c2eee89dbcad89066ab1a461ce80702f7a4a3
Zynrir <116448611+Zynrir@users.noreply.github.com>
2024-06-04 20:06:21 +0200

    Delete src/main/java/org/iesalandalus/programacion/reservashotel/modelo/negocio directory

--- src/main/java/org/iesalandalus/programacion/reservashotel/modelo/negocio/IFuenteDatos.java
+++
@@ -1,7 +0,0 @@
-package org.iesalandalus.programacion.reservashotel.modelo.negocio;
-
-public interface IFuenteDatos {
-    IHuespedes crearHuespedes();
-    IHabitaciones crearHabitaciones();
-    IReservas crearReservas();
-}
